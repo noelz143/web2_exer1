3d322f6ef8d90606baa93669948699e742af6907
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 19:34:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 		}
 	</style>
 </head>
-<body bgcolor="yellowgreen">
-	<div style="">
-	<center><h1 style="font-family: courier;"><?php echo "Hello World!";?></h1>
+<body bgcolor="#12344;">
+	<br><br><br><div style="border: 1px solid blue;height: 500px; width: 300px;margin-left: 500px;background-color:pink; border-radius: 20px;  ">
+	<center><h1 style="font-family: courier;"><?php echo "MY WORKS ";?></h1>
 	<br><a href="welcome.php"> <button style="font-size: 20px;border-radius: 12px;"> Welcome</button></a>
 	<br><br><a href="string.php"> <button style="font-size: 20px;border-radius: 12px;"> String </button> </a>
 	<br><br><a href="wspace.php"><button style="font-size: 20px;border-radius: 12px;">Wspace  </button></a>
@@ -19,6 +19,7 @@
 	<br><br><a href="post.php"> <button style="font-size: 20px;border-radius: 12px;"> Post</button></a>
 	<br><br><a href="getmethod.php"><button style="font-size: 20px;border-radius: 12px;"> Get</button></a>
 	<br><br><a href="displaymethod.php"><button style="font-size: 20px;border-radius: 12px;"> DisplayMethod</button></a>
+	
 	</div>
 </center>
 </body>
