c7e6abe55c10b735951d70e22dbed6ecf6c0e39d
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 19:35:47 -0800

    Update wspace.php

--- wspace.php
+++ wspace.php
@@ -3,8 +3,9 @@
 <head>
 	<title>Wspace</title>
 </head>
-<body>
-<center><?php 
+<body bgcolor="#12344;">
+<center> <br><br><br><div style="border: 1px solid black;height: 300px;width: 200px;border-radius: 20px; background-color: skyblue;">
+<br><?php 
 
 $four = 2 + 2;
 
@@ -13,9 +14,8 @@ echo "</br> <tab>2 + <tab>2 = <tab> $four ";
 
 
 ?></center>
-<?php 
-echo "<h1><p> this is a paragraph </p></h1>" ;
-?>
-<br><a href="index.php"> back</a>  
+
+<a href="index.php"> <button style="font-size: 20px;border-radius: 12px; margin-left: 650px;margin-top: 40px;"> Back</button></a>
+</div>
 </body>
 </html>
