2d7ca10bfd82276e5b27cf74fa51284d5a5a8a34
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 19:37:30 -0800

    Update getmethod.php

--- getmethod.php
+++ getmethod.php
@@ -14,13 +14,15 @@ if (isset($_GET["name"] )|| isset( $_GET["age"]))
 <head>
 	<title>getmethod</title>
 </head>
-<body>
-      <form action = "<?php $_PHP_SELF ?>" method = "GET">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         password: <input type="password" name="pass"/>
-         <input type = "submit" value="submit" />
+<body bgcolor="#12344;">
+<center><br><br><br><div style="border: 1px solid black;height: 400px;width: 500px;border-radius: 20px; background-color: skyblue;">
+     <br><br> <form action = "<?php $_PHP_SELF ?>" method = "GET">
+         Name:  <input style="margin-left: 20px;" type = "text" name = "name" /><br><br>
+         Age: <input style="margin-left: 30px;" type = "text" name = "age" /><br><br>
+         password  :	       <input style="margin-left: -5px;" type="password" name="pass"/><br><br>
+         <input style="font-size: 20px;border-radius: 12px;margin-left: 50px;" type = "submit" value="submit" /><br><br><br><br><br>
       </form>
-<br><a href="index.php"> back</a>
+      <a href="index.php"> <button style="font-size: 20px;border-radius: 12px;margin-left: 60px;"> Back</button></a>
+      </div> </center>
 </body>
 </html>
