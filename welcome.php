3cf4d4971fce6e79e464e3df68d2bfecb3bb29a1
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 19:39:18 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,9 +4,11 @@
 	<title>Welcome </title>
 </head>
 <?#first code in php?>
-<body bgcolor="pink">
-<center><h1><?php echo "Hello I`m Noel";?></h1>
-<br><a href="index.php"> Back</a>
+<body bgcolor="#12344;">
+<center><br><br><br><br><br><br><div style="border: 1px solid black;height: 300px;width: 200px;border-radius: 20px; background-color: skyblue;"><h1><?php echo "Hello I`m Noel";?></h1>
+<br>
+<a href="index.php"> <button style="font-size: 20px;border-radius: 12px;"> Back</button></a>
+</div>
   </center>
 
 </body>
