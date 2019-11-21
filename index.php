6dfa228df10264208e2d2dbb8f7abf671802edd0
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 18:35:55 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,24 @@
 <html>
 <head>
 	<title>Hello World</title>
+	<style>
+		button:hover{
+			background-color: #4CAF50; 
+  			color: white;
+		}
+	</style>
 </head>
 <body bgcolor="yellowgreen">
+	<div style="">
 	<center><h1 style="font-family: courier;"><?php echo "Hello World!";?></h1>
-	<a href="welcome.php"> <button style="font-size: 20px;"> Welcome</button></a>
-	<br><a href="string.php"> <button style="font-size: 20px;"> String </button> </a>
-	<br><a href="wspace.php"><button style="font-size: 20px;">Wspace  </button></a>
-	<br><a href="condition.php"> <button style="font-size: 20px;"> Condition</button> </a> 
-	<br><a href="post.php"> <button style="font-size: 20px;"> Post</button></a>
-	<br><a href="getmethod.php"><button style="font-size: 20px;"> Get</button></a>
-	<br><a href="displaymethod.php"><button style="font-size: 20px;"> DisplayMethod</button></a>
-	
+	<br><a href="welcome.php"> <button style="font-size: 20px;border-radius: 12px;"> Welcome</button></a>
+	<br><br><a href="string.php"> <button style="font-size: 20px;border-radius: 12px;"> String </button> </a>
+	<br><br><a href="wspace.php"><button style="font-size: 20px;border-radius: 12px;">Wspace  </button></a>
+	<br><br><a href="condition.php"> <button style="font-size: 20px;border-radius: 12px;"> Condition</button> </a> 
+	<br><br><a href="post.php"> <button style="font-size: 20px;border-radius: 12px;"> Post</button></a>
+	<br><br><a href="getmethod.php"><button style="font-size: 20px;border-radius: 12px;"> Get</button></a>
+	<br><br><a href="displaymethod.php"><button style="font-size: 20px;border-radius: 12px;"> DisplayMethod</button></a>
+	</div>
 </center>
 </body>
 </html>
