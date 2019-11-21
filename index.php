d296531aa6fa9a50112bb861014fee9e0a5749dd
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 18:24:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <head>
 	<title>Hello World</title>
 </head>
-<body>
-	<center><h1><?php echo "Hello World!";?></h1>
-	<a href="welcome.php"> Welcome</a>
-	<br><a href="string.php"> String</a>  
-	<br><a href="wspace.php"> Wspace</a>  
-	<br><a href="condition.php"> Condition</a>  
-	<br><a href="post.php"> Post</a>
-	<br><a href="getmethod.php"> Get</a>
+<body bgcolor="yellowgreen">
+	<center><h1 style="font-family: courier;"><?php echo "Hello World!";?></h1>
+	<a href="welcome.php"> <button style="font-size: 20px;"> Welcome</button></a>
+	<br><a href="string.php"> <button style="font-size: 20px;"> String </button> </a>
+	<br><a href="wspace.php"><button style="font-size: 20px;">Wspace  </button></a>
+	<br><a href="condition.php"> <button style="font-size: 20px;"> Condition</button> </a> 
+	<br><a href="post.php"> <button style="font-size: 20px;"> Post</button></a>
+	<br><a href="getmethod.php"><button style="font-size: 20px;"> Get</button></a>
+	<br><a href="displaymethod.php"><button style="font-size: 20px;"> DisplayMethod</button></a>
 	
 </center>
 </body>
