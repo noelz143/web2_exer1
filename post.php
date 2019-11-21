9acf53afeb4ec6d791d00a5d193a9f79f824b4b1
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-20 19:37:02 -0800

    Update post.php

--- post.php
+++ post.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>post</title>
 </head>
-<body>
-<form action = "displaymethod.php" method = "POST">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         password: <input type="password" name="pass"/>
-         <input type = "submit" value="submit" />
+<body bgcolor="#12344;">
+<center><br><br><div style="border: 1px solid black;height: 400px;width: 500px;border-radius: 20px; background-color: skyblue;">
+<br><form action = "displaymethod.php" method = "POST"><br><br>
+         Name:  <input style="margin-left: 20px;" type = "text" name = "name" /><br><br>
+         Age: <input style="margin-left: 30px;" type = "text" name = "age" /><br><br>
+         password  :	       <input style="margin-left: -5px;" type="password" name="pass"/><br><br>
+         <input style="font-size: 20px;border-radius: 12px;margin-left: 50px;" type = "submit" value="submit" /><br><br><br><br><br>
       </form>
-      <br><a href="index.php"> back</a>
-
+      <a href="index.php"> <button style="font-size: 20px;border-radius: 12px;margin-left: 60px;"> Back</button></a>
+      </div> </center>
 </body>
 </html>
