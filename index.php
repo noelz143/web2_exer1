3b6fe1bf0e17f2b81a513600018a5df1be5d9899
noelz143 <57779619+noelz143@users.noreply.github.com>
2019-11-21 17:14:41 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 	</style>
 </head>
 <body bgcolor="#12344;">
-	<br><br><br><div style="border: 1px solid blue;height: 500px; width: 300px;margin-left: 500px;background-color:pink; border-radius: 20px;  ">
+	<marquee ><h1 style="font-family: courier;color: red;">NOEL DE RAMOS</h1></marquee>
+	<br><br><br><div style="border: 1px solid black;height: 500px; width: 300px;margin-left: 500px;background-color:pink; border-radius: 20px;  ">
 	<center><h1 style="font-family: courier;"><?php echo "MY WORKS ";?></h1>
 	<br><a href="welcome.php"> <button style="font-size: 20px;border-radius: 12px;"> Welcome</button></a>
 	<br><br><a href="string.php"> <button style="font-size: 20px;border-radius: 12px;"> String </button> </a>
@@ -19,7 +20,6 @@
 	<br><br><a href="post.php"> <button style="font-size: 20px;border-radius: 12px;"> Post</button></a>
 	<br><br><a href="getmethod.php"><button style="font-size: 20px;border-radius: 12px;"> Get</button></a>
 	<br><br><a href="displaymethod.php"><button style="font-size: 20px;border-radius: 12px;"> DisplayMethod</button></a>
-	
 	</div>
 </center>
 </body>
